2414ad71168262c3caacc4fe6b9a95228e603889
espemico <emico@ieseljust.com>
2018-06-04 18:22:31 +0200

    Pujada del projecte

---
+++ Factorial/src/factorial.java
@@ -0,0 +1,22 @@
+
+import java.util.Scanner;
+ 
+public class factorial
+{    
+    private static Scanner in;
+
+	public static void main(String[] args)
+    {
+         
+        in = new Scanner(System.in);
+        int n,i=1;
+        double f=1;
+        do{
+            System.out.print("Introduir numero positiu o zero : ");
+            n=in.nextInt();
+        }while(n<0);
+        for(i=1;i<=n;i++)
+            f=f*i;
+        System.out.println("El factorial es : "+f);
+    }
+}
